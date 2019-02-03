e436bea09f49520702fe54ef437a7df0d1f43219
Piyush <piyushprasad2007@gmail.com>
2019-02-03 14:32:57 +0530

    It is similar to quartz

---
+++ src/main/java/com/javacodegeeks/example/app/App.java
@@ -0,0 +1,13 @@
+package com.javacodegeeks.example.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		String[] str = {"classpath:META-INF/spring/job-config.xml","classpath:META-INF/spring/context-config.xml"};
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(str);	
+		((AbstractApplicationContext) ctx).registerShutdownHook();
+	}
+}
---
+++ src/main/java/com/javacodegeeks/example/util/PersonModel.java
@@ -0,0 +1,43 @@
+package com.javacodegeeks.example.util;
+
+public class PersonModel {
+	private String firstName, lastName, school;
+	private int rollNumber;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public int getRollNumber() {
+		return rollNumber;
+	}
+
+	public void setRollNumber(int rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Hello, " + firstName + " " + lastName + "! Welcome to " + school + " " + rollNumber + "....";
+	}
+}
